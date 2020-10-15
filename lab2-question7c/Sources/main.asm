971506b3649c19e4c376af3560765a14aec1dfe1
Edmond Chu <pakhung.chu@ryerson.ca>
2020-10-15 00:17:52 -0400

    lab2 finished
     added code for q7 parts a,b and c aswell as q10 for lab 2.

---
+++ lab2-question7c/Sources/main.asm
@@ -0,0 +1,52 @@
+;*****************************************************************
+;* This stationery serves as the framework for a                 *
+;* user application (single file, absolute assembly application) *
+;* For a more comprehensive program that                         *
+;* demonstrates the more advanced functionality of this          *
+;* processor, please see the demonstration applications          *
+;* located in the examples subdirectory of the                   *
+;* Freescale CodeWarrior for the HC12 Program directory          *
+;*****************************************************************
+
+; export symbols
+        XDEF Entry, _Startup            ; export 'Entry' symbol
+        ABSENTRY Entry        ; for absolute assembly: mark this as application entry point
+
+
+
+; Include derivative-specific definitions 
+        INCLUDE 'derivative.inc' ;code starts below 
+
+;**
+;* Code section                                                    *
+;**
+
+
+; variable/data section
+
+            ORG $3000
+
+;**
+;* Actual Code starts here                                         *
+;**
+
+            ORG   $4000
+
+Entry:
+_Startup:
+          
+          BSET DDRP,%11111111 ; Config. Port P for output
+          LDAA #%10000000 ; Prepare to drive PP7 high
+
+MainLoop  STAA PTP ; Drive PP7
+          LDX #$1FFF ; Initialize the loop counter
+Delay     DEX ; Decrement the loop counter
+          BNE Delay ; If not done, continue to loop
+          EORA #%10000000 ; Toggle the MSB of AccA
+          BRA MainLoop ; Go to MainLoop
+;**
+;Interrupt Vectors                          *
+;**
+         ORG  $FFFE
+         FDB  Entry           
+; Reset Vector
